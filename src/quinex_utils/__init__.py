2c21d8cc76f2b6e73371b8475ff77a8469f0cab4
Jan Göpfert <j.goepfert@fz-juelich.de>
2025-10-23 18:36:36 +0200

    Fixed path to static resources

--- src/quinex_utils/__init__.py
+++ src/quinex_utils/__init__.py
@@ -1,7 +1,6 @@
-import os
 from pathlib import Path
 
-project_dir = Path(__file__).resolve().parents[2]
+
+MODULE_DIR = Path(__file__).resolve().parent
 CONFIG = {}
-CONFIG["project_dir"] = str(project_dir)
-CONFIG["static_resources_dir"] = os.path.join(CONFIG["project_dir"], "src/quinex_utils/parsers/static_resources/")
+CONFIG["static_resources_dir"] = MODULE_DIR / "parsers/static_resources/"
